9808a335ea2e0f31eb41a288d414af6bd9345d10
draka <drakanoy@gmail.com>
2022-10-05 00:01:56 +0300

    добавили коментарий РЕР-8 наше всё

--- seminar_1/program_1.py
+++ seminar_1/program_1.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as mpp
 if __name__ == '__main__':  # начало программы
     arguments = numpy.arange(0, 200, 0.1)  # список большого количества точек)
     mpp.plot(
-        arguments,
+        arguments,  # конкретно мне не очень нравится так аргументы в функцию передавать, но раз в столбик, то в столбик
         [math.sin(a) * math.sin(a / 20.0) for a in arguments]
     )  # строим по точкам функцию
     mpp.show()  # выводим картинку
