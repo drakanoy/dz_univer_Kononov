f332fd6e97fc88ce13eefbc1b19590e2b2318375
draka <drakanoy@gmail.com>
2022-11-20 21:08:28 +0300

    Евклид расширенный

--- efklid.py
+++ efklid.py
@@ -1,4 +1,6 @@
 import math
+import numpy
+
 
 def gcd(a, b):
     while b != 0:
@@ -6,6 +8,19 @@ def gcd(a, b):
     return a
 
 
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        div, x, y = egcd(b % a, a)
+    return (div, y - (b // a) * x, x)
+
+
 a = int(input())
 b = int(input())
+print(gcd(a, b))
 print(math.gcd(a, b) == gcd(a, b))
+
+d, x, y = egcd(a, b)
+print(d, x, y)
+print(d == a * x + b * y)
