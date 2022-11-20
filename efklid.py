38b9ee39fa96408b881e6f970981b0fbc0ee09b6
draka <drakanoy@gmail.com>
2022-11-20 21:18:23 +0300

    Евклид расширенный

--- efklid.py
+++ efklid.py
@@ -22,8 +22,9 @@ def egcd(a, b):
 a = int(input())
 b = int(input())
 print(gcd(a, b))
-print(math.gcd(a, b) == gcd(a, b))
+print(math.gcd(a, b) == gcd(a, b))  # проверка с бибеотекой math
 
 d, x, y = egcd(a, b)
+print(d == gcd(a, b))  # проверка на одинаковость наибольших делителей
 print(d, x, y)
-print(d == a * x + b * y)
+print(d == a * x + b * y)  # проверка равно ли разлодение самому делителю
