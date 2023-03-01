5414dbb3a29cdd1c38073688e8d281ce699f5a38
draka <drakanoy@gmail.com>
2023-03-01 03:22:20 +0300

    проверим скорость моего "метода Гаусса"

--- gauss.py
+++ gauss.py
@@ -2,6 +2,7 @@ from numpy import array, concatenate
 from numpy.linalg import norm
 from numpy.linalg import solve as solve_out_of_the_box
 from proverka_gauss import test_error
+import timeit
 
 a = array([
     [1.5, 2.0, 1.5, 2.0],
@@ -46,3 +47,6 @@ oob_solution = solve_out_of_the_box(a, b)
 print(solution)
 print("Макс отклонение компоненты решения:", norm(solution - oob_solution, ord=1))
 print(test_error(vector_gauss))
+
+print('Мой метод Гаусса', timeit.timeit(lambda: test_error(vector_gauss), number=10) / 10)
+print('Встроенный метод Гаусса', timeit.timeit(lambda: test_error(solve_out_of_the_box), number=10) / 10)
