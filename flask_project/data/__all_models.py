0503138025689dd6968415d23fd6c031e76f1a30
drakanoy <75383693+drakanoy@users.noreply.github.com>
2023-05-17 11:34:58 +0300

    Update __all_models.py
    :)

--- flask_project/data/__all_models.py
+++ flask_project/data/__all_models.py
@@ -2,7 +2,7 @@ from . import users
 from . import compositions
 
 # если вы читаете этот коментари1, отпишитесь нам, выдадим 100 рублей
-# ps наша почта sashailyin2006@gmail.com :)
+# ps наша почта drakanoy@gmail.com :)
 
 def something():
     # to do something
