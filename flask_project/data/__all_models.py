cc984f8de2e43da53ba50f7039865c9102a44266
draka <drakanoy@gmail.com>
2023-05-17 22:46:54 +0300

    :)

--- flask_project/data/__all_models.py
+++ flask_project/data/__all_models.py
@@ -1,8 +1,8 @@
 from . import users
 from . import compositions
 
-# если вы читаете этот коментари1, отпишитесь нам, выдадим 100 рублей
-# ps наша почта drakanoy@gmail.com :)
+# если вы читаете этот комментарий, отпишитесь мне, выдадим 100 рублей
+# ps моя почта drakanoy@gmail.com :)
 
 def something():
     # to do something
